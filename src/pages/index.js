f66abddd20a93a539554a41c23acaa70690c6c02
Natalia Dominique <nattalia.xd@gmail.com>
2023-09-25 09:29:42 -0300

    5 colores

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,33 +5,36 @@ import Colors from "@/components/Colors/Colors";
 import { useState, useEffect } from "react";
 
 const poppins = Poppins({
-  weight: ["200", "300", "400", "500", "700"],
-  subsets: ["latin-ext"],
+    weight: ["200", "300", "400", "500", "700"],
+    subsets: ["latin-ext"],
 });
 
 export async function getServerSideProps() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_IP}/api/colors`);
-  const colors = await res.json();
-  const values = colors.values;
+    const res = await fetch(`${process.env.NEXT_PUBLIC_IP}/api/colors`);
+    const colors = await res.json();
+    const values = colors.values;
 
-  return {
-    props: {
-      colors: values,
-    },
-  };
+    return {
+        props: {
+            colors: values,
+        },
+    };
 }
 
 export default function Home({ colors }) {
-  return (
-    <main className={`flex min-h-screen flex-wrap justify-center items-center p-12 ${poppins.className}`}>
-      <Drawer>
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-4xl font-bold text-white text-center">Filtros</h1>
-          <p className="text-white">Lorem</p>
-        </div>
-      </Drawer>
-      <ChangeBG />
-      <Colors colors={colors} />
-    </main>
-  );
+    return (
+        <main
+            className={`flex min-h-screen flex-wrap justify-center items-center p-12 ${poppins.className} custom-bg`}
+        >
+            <Drawer>
+                <div className="flex flex-col items-center justify-center">
+                    <h1 className="text-4xl font-bold text-white text-center">
+                        Filtros
+                    </h1>
+                    <p className="text-white">Lorem</p>
+                </div>
+            </Drawer>
+            <Colors colors={colors} />
+        </main>
+    );
 }
