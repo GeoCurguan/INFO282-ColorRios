a951118da39d69194014c3c4c3550e6274051647
geovanni <curguangeo@gmail.com>
2023-10-31 16:10:51 -0300

    pdf con estilo del boceto

--- src/components/ExportPDF/ExportPDF.js
+++ src/components/ExportPDF/ExportPDF.js
@@ -6,12 +6,60 @@ import { PDFDownloadLink, Document, Page, Text, View, StyleSheet } from '@react-
 const styles = StyleSheet.create({
   page: {
     flexDirection: 'row',
-    backgroundColor: '#E4E4E4'
+    padding: '40',
   },
   section: {
-    margin: 10,
-    padding: 10,
+	border: '2px solid black',
     flexGrow: 1
+  },
+  cardColor: {
+    backgroundColor: '#D1D5DB',
+    height: '40%',
+    padding: '40 40 50 40',
+  },
+  selectedColor: {
+    height: '100%',
+	width: '100%',
+    backgroundColor: '#434343', //Cambiar a variable para que se adapte al color
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  colorImage: {
+    backgroundColor: 'gray', //Cambiar a backgroundImage de la imagen asignada para el color
+    marginTop: '150px',
+    width: '210',
+    height: '210',
+    borderRadius: '100',
+    border: '5px solid #434343',
+    position: 'absolute',
+	bottom: '-30px'
+  },
+  cardInfo: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  colorTitle: {
+    fontSize: '32px',
+  },
+  colorLocation: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-evenly',
+  },
+  colorLocationData: {
+    width: '40%',
+    alignItems: 'center',
+    
+  },
+  dataContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    padding: '10px 5px 10px 5px',
+    margin : '10px 0px 10px 0px',
+  },
+  subTitles: {
+    fontSize: '26px',
   }
 });
 
@@ -21,15 +69,142 @@ const PDFToExport = ({colorPalette}) => {
     <Document>
     <Page size="A4" style={styles.page}>
       <View style={styles.section}>
-        <Text>Section #1</Text>
+        <View style={styles.cardColor}>
+          <View style={styles.selectedColor}>
+            <View style={styles.colorImage}>
+            </View>
+          </View>
+        </View>
+        <View style={styles.cardInfo}>
+          <Text style={styles.colorTitle}>Nombre de color</Text>
+          <Text style={styles.subTitles}>Informacion</Text>
+          <View style={styles.colorLocation}>
+            <View style={styles.colorLocationData}>
+              <View style={styles.dataContainer}>
+                <Text style={styles.subTitles}>Comuna</Text>
+              	<Text>xxx -xxx -xxx</Text>
+              </View>
+
+            </View>
+  	        <View style={styles.colorLocationData}>
+            	<View style={styles.dataContainer}>
+            		<Text style={styles.subTitles}>Muestra</Text>
+                	<Text>xxx -xxx -xxx</Text>
+              	</View>
+            </View>
+          </View>
+          <Text style={styles.subTitles}>Códigos</Text>
+          	<View style={styles.colorLocation}>
+              <View style={styles.colorLocation}>
+              	<View style={styles.colorLocationData}>
+                  <View style={styles.dataContainer}>
+                    <Text style={styles.subTitles}>NCS</Text>
+                  	<Text>xxx -xxx -xxx</Text>
+                  </View>
+
+                  <View style={styles.dataContainer}>
+                    <Text style={styles.subTitles}>Cielab</Text>
+                    <Text>xxx -xxx -xxx</Text>
+                  </View>
+                  
+                  <View style={styles.dataContainer}>
+                    <Text style={styles.subTitles}>Cmynk</Text>
+                    <Text>xxx -xxx -xxx</Text>
+                  </View>
+              	</View>
+              	<View style={styles.colorLocationData}>
+                  <View style={styles.dataContainer}>
+                    <Text style={styles.subTitles}>RGB</Text>
+                    <Text>xxx -xxx -xxx</Text>
+                  </View>
+                  
+                  <View style={styles.dataContainer}>
+                  	<Text style={styles.subTitles}>Munsell</Text>
+                  	<Text>xxx -xxx -xxx</Text>
+                  </View>
+                  
+                  
+                  <View style={styles.dataContainer}>
+                  	<Text style={styles.subTitles}>Cerecita</Text>
+                  	<Text>xxx -xxx -xxx</Text>
+                  </View>
+
+              	</View>
+            </View>
+          </View>
+        </View>
       </View>
+    </Page>
+        <Page size="A4" style={styles.page}>
       <View style={styles.section}>
-        <Text>Section #2</Text>
+        <View style={styles.cardColor}>
+          <View style={styles.selectedColor}>
+            <View style={styles.colorImage}>
+            </View>
+          </View>
+        </View>
+        <View style={styles.cardInfo}>
+          <Text style={styles.colorTitle}>Nombre de color</Text>
+          <Text style={styles.subTitles}>Informacion</Text>
+          <View style={styles.colorLocation}>
+            <View style={styles.colorLocationData}>
+              <View style={styles.dataContainer}>
+                <Text style={styles.subTitles}>Comuna</Text>
+              	<Text>xxx -xxx -xxx</Text>
+              </View>
+
+            </View>
+  	        <View style={styles.colorLocationData}>
+            	<View style={styles.dataContainer}>
+            		<Text style={styles.subTitles}>Muestra</Text>
+                	<Text>xxx -xxx -xxx</Text>
+              	</View>
+            </View>
+          </View>
+          <Text style={styles.subTitles}>Códigos</Text>
+          	<View style={styles.colorLocation}>
+              <View style={styles.colorLocation}>
+              	<View style={styles.colorLocationData}>
+                  <View style={styles.dataContainer}>
+                    <Text style={styles.subTitles}>NCS</Text>
+                  	<Text>xxx -xxx -xxx</Text>
+                  </View>
+
+                  <View style={styles.dataContainer}>
+                    <Text style={styles.subTitles}>Cielab</Text>
+                    <Text>xxx -xxx -xxx</Text>
+                  </View>
+                  
+                  <View style={styles.dataContainer}>
+                    <Text style={styles.subTitles}>Cmynk</Text>
+                    <Text>xxx -xxx -xxx</Text>
+                  </View>
+              	</View>
+              	<View style={styles.colorLocationData}>
+                  <View style={styles.dataContainer}>
+                    <Text style={styles.subTitles}>RGB</Text>
+                    <Text>xxx -xxx -xxx</Text>
+                  </View>
+                  
+                  <View style={styles.dataContainer}>
+                  	<Text style={styles.subTitles}>Munsell</Text>
+                  	<Text>xxx -xxx -xxx</Text>
+                  </View>
+                  
+                  
+                  <View style={styles.dataContainer}>
+                  	<Text style={styles.subTitles}>Cerecita</Text>
+                  	<Text>xxx -xxx -xxx</Text>
+                  </View>
+
+              	</View>
+            </View>
+          </View>
+        </View>
       </View>
     </Page>
   </Document>
-  )
-
+);
 }
 
 //Componente del boton de el export
@@ -46,7 +221,7 @@ const ExportPDF = () => {
       {
         isClient ?
           <div>
-          <PDFDownloadLink document={<PDFToExport />} fileName="somename.pdf">
+          <PDFDownloadLink document={<PDFToExport />} fileName="colores-favoritos.pdf">
             {({ blob, url, loading, error }) => (loading ? 'Cargando colores...' : 'Exportar colores')}
           </PDFDownloadLink>
           </div> : <></>
