4dc84e299d4ec3ebef546ed63ae54119fc0684a0
alexbgh1 <alex.garnica@alumnos.uach.cl>
2023-12-02 12:58:56 -0300

    Navegacion responsive en lado izq, falta el lado derecho

--- frontend/src/components/Navbar/NavAccount.jsx
+++ frontend/src/components/Navbar/NavAccount.jsx
@@ -5,13 +5,14 @@ const NavAccount = ({ isActive, user }) => {
   console.log("user", user);
   const activeClassDark = "bg-[#434343] text-[#D9D9D9] rounded-full";
   return (
-    <ul className="py-4 px-8 gap-8 w-1/2 flex justify-evenly items-center">
+    <ul className="py-4 px-8 gap-2 md:gap-8 w-1/2 flex justify-evenly items-center">
       <li className={`px-4 py-2 ${isActive("/social") && activeClassDark}`}>
         <Link href="/social">Social</Link>
       </li>
       {user?.username ? (
-        <Link href="/perfil">
-          {/* <Image
+        <li>
+          <Link href="/perfil">
+            {/* <Image
             // alt={session?.user?.name || "User Image"}
             priority
             width={40}
@@ -19,12 +20,15 @@ const NavAccount = ({ isActive, user }) => {
             // src={session?.user?.image || "/images/placeholder/user-placeholder.png"}
             className="cursor-pointer object-cover w-10 h-10 rounded-full"
           /> */}
-          <div className="w-10 h-10 rounded-full bg-gradient-to-t from-red-500 to-yellow-500 border-2 border-[#434343]"></div>
-        </Link>
+            <div className="w-10 h-10 rounded-full bg-gradient-to-t from-red-500 to-yellow-500 border-2 border-[#434343]"></div>
+          </Link>
+        </li>
       ) : (
-        <Link className="px-4 py-2 rounded-full" href={`/login`}>
-          Login
-        </Link>
+        <li>
+          <Link className="px-4 py-2 rounded-full" href={`/login`}>
+            Login
+          </Link>
+        </li>
       )}
     </ul>
   );
--- frontend/src/pages/perfil/index.js
+++ frontend/src/pages/perfil/index.js
@@ -6,12 +6,20 @@ import ProfileCard from "@/components/profile/ProfileCard";
 
 const Page = () => {
   // TOOD: PERFIL - RUTA PROTEGIDA
-  const { user } = useAuthContext();
+  const { user, handleLogout } = useAuthContext();
   return (
     <ProtectedLogged>
       <Nav />
       <div className="mt-8 flex flex-col flex-wrap justify-center items-center">
         <ProfileCard user={user} />
+      </div>
+      <div className="flex justify-center items-center">
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-8"
+          onClick={handleLogout}
+        >
+          Cerrar sesión
+        </button>
       </div>
     </ProtectedLogged>
   );
