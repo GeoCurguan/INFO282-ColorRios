3968a7c1744dca82731dd72215c77c2e3253e110
geovanni <curguangeo@gmail.com>
2023-12-05 16:22:30 -0300

    JSON de credentials + validaciones al insert + rowId en getcolor

--- backend/src/Controller/ColorController.php
+++ backend/src/Controller/ColorController.php
@@ -29,7 +29,9 @@ class ColorController extends AbstractController
         //Acceder a la variable de entorno y decodificar el JSON
         $googleAuthConfig = json_decode($_ENV['GOOGLE_AUTH_CONFIG'] ?? '{}', true);
 
-        $client->setAuthConfig($googleAuthConfig);
+        //$client->setAuthConfig($googleAuthConfig);
+        $client->setAuthConfig(__DIR__ . '/credentials.json');
+
         $service = new Google_Service_Sheets($client);
         $spreadsheetId = "1n2IdxzwSSO8mXaeZI8p7j2ZFZXObtBD_NPjjnUls6yU";
 
@@ -72,18 +74,18 @@ class ColorController extends AbstractController
                     $color->setMunsellChroma($value[30] ? $value[30] : null);
                     $color->setMunsellName($value[31] ? $value[31] : null);
                     //CIELAB
-                    $color->setCielabL($value[32] ? gettype($value[32]) !== 'integer' ? null : $value[32] : null);
-                    $color->setCielabA($value[33] ? gettype($value[33]) !== 'integer' ? null : $value[33] : null);
-                    $color->setCielabB($value[34] ? gettype($value[34]) !== 'integer' ? null : $value[34] : null);
+                    $color->setCielabL($value[32] ? gettype($value[32]) !== 'integer' ? intval($value[32])  : $value[32] : null);
+                    $color->setCielabA($value[33] ? gettype($value[33]) !== 'integer' ? intval($value[33])  : $value[33] : null);
+                    $color->setCielabB($value[34] ? gettype($value[34]) !== 'integer' ? intval($value[34])  : $value[34] : null);
                     //RGB
-                    $color->setRgbR($value[36] ? gettype($value[36]) !== 'integer' ? null : $value[36] : null);
-                    $color->setRgbG($value[37] ? gettype($value[37]) !== 'integer' ? null : $value[37] : null);
-                    $color->setRgbB($value[38] ? gettype($value[38]) !== 'integer' ? null : $value[38] : null);
+                    $color->setRgbR($value[36] ? gettype($value[36]) !== 'integer' ? intval($value[36]) : $value[36] : null);
+                    $color->setRgbG($value[37] ? gettype($value[37]) !== 'integer' ? intval($value[37]) : $value[37] : null);
+                    $color->setRgbB($value[38] ? gettype($value[38]) !== 'integer' ? intval($value[38]) : $value[38] : null);
                     //CMYK
-                    $color->setCmykC($value[39] ? gettype($value[39]) !== 'integer' ? null : $value[39] : null);
-                    $color->setCmykM($value[40] ? gettype($value[40]) !== 'integer' ? null : $value[40] : null);
-                    $color->setCmykY($value[41] ? gettype($value[41]) !== 'integer' ? null : $value[41] : null);
-                    $color->setCmykK($value[42] ? gettype($value[42]) !== 'integer' ? null : $value[42] : null);
+                    $color->setCmykC($value[39] ? gettype($value[39]) !== 'integer' ? intval($value[39])  : $value[39] : null);
+                    $color->setCmykM($value[40] ? gettype($value[40]) !== 'integer' ? intval($value[40])  : $value[40] : null);
+                    $color->setCmykY($value[41] ? gettype($value[41]) !== 'integer' ? intval($value[41])  : $value[41] : null);
+                    $color->setCmykK($value[42] ? gettype($value[42]) !== 'integer' ? intval($value[42])  : $value[42] : null);
                     //FILTRO POR NOMBRE DE COLOR
                     $color->setCategoryName(isset($value[46]) ? $value[46] : null);
 
@@ -101,18 +103,18 @@ class ColorController extends AbstractController
                     $get_color->setMunsellChroma($value[30] ? $value[30] : null);
                     $get_color->setMunsellName($value[31] ? $value[31] : null);
                     //CIELAB
-                    $get_color->setCielabL($value[32] ? gettype($value[32]) !== 'integer' ? null : $value[32] : null);
-                    $get_color->setCielabA($value[33] ? gettype($value[33]) !== 'integer' ? null : $value[33] : null);
-                    $get_color->setCielabB($value[34] ? gettype($value[34]) !== 'integer' ? null : $value[34] : null);
+                    $get_color->setCielabL($value[32] ? gettype($value[32]) !== 'integer' ? intval($value[32])  : $value[32] : null);
+                    $get_color->setCielabA($value[33] ? gettype($value[33]) !== 'integer' ? intval($value[33])  : $value[33] : null);
+                    $get_color->setCielabB($value[34] ? gettype($value[34]) !== 'integer' ? intval($value[34])  : $value[34] : null);
                     //RGB
-                    $get_color->setRgbR($value[36] ? gettype($value[36]) !== 'integer' ? null : $value[36] : null);
-                    $get_color->setRgbG($value[37] ? gettype($value[37]) !== 'integer' ? null : $value[37] : null);
-                    $get_color->setRgbB($value[38] ? gettype($value[38]) !== 'integer' ? null : $value[38] : null);
+                    $get_color->setRgbR($value[36] ? gettype($value[36]) !== 'integer' ? intval($value[36]) : $value[36] : null);
+                    $get_color->setRgbG($value[37] ? gettype($value[37]) !== 'integer' ? intval($value[37]) : $value[37] : null);
+                    $get_color->setRgbB($value[38] ? gettype($value[38]) !== 'integer' ? intval($value[38]) : $value[38] : null);
                     //CMYK
-                    $get_color->setCmykC($value[39] ? gettype($value[39]) !== 'integer' ? null : $value[39] : null);
-                    $get_color->setCmykM($value[40] ? gettype($value[40]) !== 'integer' ? null : $value[40] : null);
-                    $get_color->setCmykY($value[41] ? gettype($value[41]) !== 'integer' ? null : $value[41] : null);
-                    $get_color->setCmykK($value[42] ? gettype($value[42]) !== 'integer' ? null : $value[42] : null);
+                    $get_color->setCmykC($value[39] ? gettype($value[39]) !== 'integer' ? intval($value[39])  : $value[39] : null);
+                    $get_color->setCmykM($value[40] ? gettype($value[40]) !== 'integer' ? intval($value[40])  : $value[40] : null);
+                    $get_color->setCmykY($value[41] ? gettype($value[41]) !== 'integer' ? intval($value[41])  : $value[41] : null);
+                    $get_color->setCmykK($value[42] ? gettype($value[42]) !== 'integer' ? intval($value[42])  : $value[42] : null);
                     //FILTRO POR NOMBRE DE COLOR
                     $get_color->setCategoryName($value[46] ? $value[46] : null);
                 }
@@ -157,6 +159,7 @@ class ColorController extends AbstractController
                 'Y' => $color->getCmykY(),
                 'K' => $color->getCmykK(),
                 'Ceresita' => $color->getCeresitaName(),
+                'rowId' => $color->getRowID(),
             ];
         }
 
