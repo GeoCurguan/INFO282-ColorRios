684fd71d5a01669c513457b2cceb3d93cf98df97
Sebastián Oyarzun <82045573+PanKekeCL@users.noreply.github.com>
2023-12-05 17:08:14 -0300

    Cambios de Palettes
    Tambien arregle un error en Color Controller.
    Casi todos los cambios aqui son sobre Palettes.

---
+++ backend/src/Controller/PaletteController.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Palette;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PaletteController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function insertPalette(Request $request): Response
+    {
+        // Assuming you have the necessary data in the request, adapt as needed
+        $data = json_decode($request->getContent(), true);
+
+        $palette = new Palette();
+        $palette->setUserId($data['id_usuario']);
+        $palette->setColorCount($data['cantidad_colores']);
+        $palette->setDownloaded($data['descargado']);
+
+        $this->entityManager->persist($palette);
+        $this->entityManager->flush();
+
+        return new JsonResponse(['message' => 'Palette inserted successfully'], Response::HTTP_OK);
+    }
+
+    public function getPalettes(): JsonResponse
+    {
+        $paletteRepository = $this->entityManager->getRepository(Palette::class);
+        $palettes = $paletteRepository->findAll();
+
+        $palettesArray = [];
+
+        foreach ($palettes as $palette) {
+            $palettesArray[] = [
+                'id' => $palette->getId(),
+                'id_usuario' => $palette->getUserId(),
+                'cantidad_colores' => $palette->getColorCount(),
+                'descargado' => $palette->getDownloaded(),
+            ];
+        }
+
+        return new JsonResponse(['palettes' => $palettesArray], Response::HTTP_OK);
+    }
+
+    // Add other methods as needed for specific functionalities related to the Palette entity
+
+    public function getTopPalettes(): JsonResponse
+    {
+        // Busca el repositorio
+        $paletteRepository = $this->entityManager->getRepository(Palette::class);
+
+        // Hace la consulta: Entrega los colores de las paletas con mas ???.
+        $result = $paletteRepository->findTopPalettes();
+
+        $palettesArray = [];
+        foreach ($result as $row) {
+            $paletteId = $row['paletteId']; // Estos nombres son definidos en la consulta del repository.
+            $color = [
+                'id' => $row['colorId'],    // Estos nombres son definidos en la consulta del repository.
+                'category' => $row['category'],
+                'commune' => $row['commune'],
+                'season' => $row['season'],
+                'colorName' => $row['colorName'],
+                'image' => $row['image'],
+                'ncsNuance' => $row['ncsNuance'],   // Si algo no funciona es porque le quite la primera mayuscula a NCS, Munsell y Ceresita.
+                'ncsHue' => $row['ncsHue'],
+                'munsellPage' => $row['munsellPage'],
+                'munsellHue' => $row['munsellHue'],
+                'munsellValue' => $row['munsellValue'],
+                'munsellChroma' => $row['munsellChroma'],
+                'munsellName' => $row['munsellName'],
+                'L*' => $row['cielabL'],
+                'A*' => $row['cielabA'],
+                'B*' => $row['cielabB'],
+                'R' => $row['rgbR'],
+                'G' => $row['rgbG'],
+                'B' => $row['rgbB'],
+                'C' => $row['cmykC'],
+                'M' => $row['cmykM'],
+                'Y' => $row['cmykY'],
+                'K' => $row['cmykK'],
+                'ceresita' => $row['ceresitaName'],
+            ];
+            $palettesArray[$paletteId][] = $color;  // Guarda este color dentro de su paleta correspondiente.
+        }
+        $palettesArray = array_values($palettesArray);  // Se supone que esto elimina espacios vacios dentro de la lista.
+        return new JsonResponse(['palettes' => $palettesArray], Response::HTTP_OK);
+    }
+
+
+
+
+
+    public function getPalettesByUsername(string $username): JsonResponse
+    {
+        // Busca el repositorio
+        $paletteRepository = $this->entityManager->getRepository(Palette::class);
+
+        // Hace la consulta: Entrega hasta 10 paletas con mas ???.
+        $result = $paletteRepository->findPalettesByUsername($username);
+
+        $palettesArray = [];
+        foreach ($result as $row) {
+            $paletteId = $row['paletteId']; // Estos nombres son definidos en la consulta del repository.
+            $color = [
+                'id' => $row['colorId'],    // Estos nombres son definidos en la consulta del repository.
+                'category' => $row['category'],
+                'commune' => $row['commune'],
+                'season' => $row['season'],
+                'colorName' => $row['colorName'],
+                'image' => $row['image'],
+                'ncsNuance' => $row['ncsNuance'],   // Si algo no funciona es porque le quite la primera mayuscula a NCS, Munsell y Ceresita.
+                'ncsHue' => $row['ncsHue'],
+                'munsellPage' => $row['munsellPage'],
+                'munsellHue' => $row['munsellHue'],
+                'munsellValue' => $row['munsellValue'],
+                'munsellChroma' => $row['munsellChroma'],
+                'munsellName' => $row['munsellName'],
+                'L*' => $row['cielabL'],
+                'A*' => $row['cielabA'],
+                'B*' => $row['cielabB'],
+                'R' => $row['rgbR'],
+                'G' => $row['rgbG'],
+                'B' => $row['rgbB'],
+                'C' => $row['cmykC'],
+                'M' => $row['cmykM'],
+                'Y' => $row['cmykY'],
+                'K' => $row['cmykK'],
+                'ceresita' => $row['ceresitaName'],
+            ];
+            $palettesArray[$paletteId][] = $color;  // Guarda este color dentro de su paleta correspondiente.
+        }
+        $palettesArray = array_values($palettesArray);  // Se supone que esto elimina espacios vacios dentro de la lista.
+        return new JsonResponse(['palettes' => $palettesArray], Response::HTTP_OK);
+    }
+}
--- frontend/src/components/Top/TopPalettes.js
+++ frontend/src/components/Top/TopPalettes.js
@@ -1,55 +1,95 @@
 import React, { useState, useEffect } from 'react';
 
 const TopPalettes = ({ username }) => {
-  const [topPalettes, setTopColors] = useState([]);
+  const [Palettes, setPalettes] = useState([]);
 
   useEffect(() => {
 
-  }, []);
-    
+    const fetchData = async () => {
+      // TopPalettes por Click
+      try {
+        const response = await fetch('/api/topPalettes');
+        if (!response.ok) {
+          throw new Error('Error al obtener las paletas mas populares.');
+        }
+
+        const data = await response.json();
+        setPalettes(data.palettes);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    // Usado en la pagina Perfil
+    const fetchDataUsername = async () => {
+      // Paletas por Usuario
+      try {
+        const response = await fetch('/api/getPalettes/${username}');
+        if (!response.ok) {
+          throw new Error('Error al obtener las paletas del usuario ${username}.');
+        }
+
+        const data = await response.json();
+        setPalettes(data.palettes);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+
+    if (username) { // Llama a una funcion dependiendo si existe usuario o no.
+      const title = "Tus Paletas"
+      fetchDataUsername();
+    } else {
+      const title = "Paletas Populares"
+      fetchData();
+    }
+
+  }, [username]); //Hook. Cuando estas variables cambien, se vuelve a ejecutar el useEffect.
+
+  // Definir funcion para construir las paletas.
+
+  function showPalettes(palettes) {
+    return (
+      <div>
+        {palettes.map((palette, index) => (
+          <div key={index} class="w-full p-4">
+            <h1>{`Paleta Numero ${index + 1}`}</h1>
+            <div class="flex items-center space-x-1">
+              {showColors(palette)}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  function showColors(colors) {
+    if (colors.length === 1) {
+      // Si el array colors tiene solo un color
+      const divColor = `rgb(${colors[0]["R"]}, ${colors[0]["G"]}, ${colors[0]["B"]})`;
+      return (
+        <div style={{ backgroundColor: divColor }} className="h-14 w-full rounded"></div>
+      );
+    } else {
+      // Si el array colors tiene más de un color
+      return colors.map((color, index) => {
+        const divColor = `rgb(${colors[index]["R"]}, ${colors[index]["G"]}, ${colors[index]["B"]})`;
+        return (
+          <div
+            key={index}
+            style={{ backgroundColor: divColor }}
+            className={`h-14 w-full ${index === 0 ? 'rounded-l' : ''} ${index === colors.length - 1 ? 'rounded-r' : ''}`}
+          ></div>
+        );
+      });
+    }
+  }
+
   return (
     <div class="rounded-xl bg-gray-300 shadow-md">
-      <div class="w-full h-12 rounded-t-xl bg-gray-100 shadow-md flex items-center justify-center text-center text-2xl font-bold">Paletas populares</div>
+      <div class="w-full h-12 rounded-t-xl bg-gray-100 shadow-md flex items-center justify-center text-center text-2xl font-bold">{title}</div>
 
-      <div class="w-full p-4">
-        <h1>Paleta Numero 1</h1>
-        <div class="flex items-center space-x-1">
-          <div class="h-14 w-full rounded bg-red-500"></div>
-        </div>
-      </div>
-
-      <div class="w-full p-4">
-        <h1>Paleta Numero 2</h1>
-        <div class="flex items-center space-x-1">
-          <div class="h-14 w-full rounded-l bg-red-500"></div>
-          <div class="h-14 w-full rounded-r bg-red-500"></div>
-        </div>
-      </div>
-
-      <div class="w-full p-4">
-        <h1>Paleta Numero 3</h1>
-        <div class="flex items-center space-x-1">
-          <div class="h-14 w-full rounded-l bg-red-500"></div>
-          <div class="h-14 w-full bg-red-500"></div>
-          <div class="h-14 w-full rounded-r bg-red-500"></div>
-        </div>
-      </div>
-
-      <div class="w-full p-4">
-        <h1>Paleta Numero 4</h1>
-        <div class="flex items-center space-x-1">
-          <div class="h-14 w-full rounded-l bg-red-500"></div>
-          <div class="h-14 w-full bg-red-500"></div>
-          <div class="h-14 w-full bg-red-500"></div>
-          <div class="h-14 w-full bg-red-500"></div>
-          <div class="h-14 w-full bg-red-500"></div>
-          <div class="h-14 w-full bg-red-500"></div>
-          <div class="h-14 w-full bg-red-500"></div>
-          <div class="h-14 w-full bg-red-500"></div>
-          <div class="h-14 w-full bg-red-500"></div>
-          <div class="h-14 w-full rounded-r bg-red-500"></div>
-        </div>
-      </div>
+      {showPalettes(palettes)}
 
     </div>);
 }
