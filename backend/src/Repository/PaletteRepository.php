c923a11677e422b7f1d4c239052467846d966f17
Sebastián Oyarzun <82045573+PanKekeCL@users.noreply.github.com>
2023-12-09 15:42:12 -0300

    Update PaletteRepository.php

--- backend/src/Repository/PaletteRepository.php
+++ backend/src/Repository/PaletteRepository.php
@@ -21,6 +21,18 @@ class PaletteRepository extends ServiceEntityRepository
         parent::__construct($registry, Palette::class);
     }
 
+    public function findPalettesByUsername($username)
+    {
+        return $this->createQueryBuilder('p')
+            ->select('p.id AS paletteId, c.id AS colorId, c.category, c.commune, c.seasons, c.colorName, c.image, c.ncsNuance, c.ncsHue, c.munsellPage, c.munsellHue, c.munsellValue, c.munsellChroma, c.munsellName, c.cielabL, c.cielabA, c.cielabB, c.rgbR, c.rgbG, c.rgbB, c.cmykC, c.cmykM, c.cmykY, c.cmykK, c.ceresitaName, c.categoryName, c.rowId')
+            ->innerJoin('p.paletteColors', 'pc')
+            ->innerJoin('pc.color', 'c')
+            ->where('p.username = :username')
+            ->setParameter('username', $username)
+            ->getQuery()
+            ->getResult();
+    }
+
 //    /**
 //     * @return Palette[] Returns an array of Palette objects
 //     */
