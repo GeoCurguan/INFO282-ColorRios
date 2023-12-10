91f007a2bd6b828cc03cdb55780ffa8531bef6b8
Geovanni Curguan <curguangeo@gmail.com>
2023-12-10 15:05:47 -0300

    Imagen removida en export de pdf + iconos para export y guardar

--- frontend/src/components/ExportPDF/ExportPDF.js
+++ frontend/src/components/ExportPDF/ExportPDF.js
@@ -28,7 +28,7 @@ const styles = StyleSheet.create({
     width: "220",
     height: "220",
     borderRadius: "400",
-    border: "10px solid #434343", //Cambiar a variable para que se adapte al color
+    border: "10px solid #D1D5DB", //Cambiar a variable para que se adapte al color
     position: "absolute",
     bottom: "-30px",
     overflow: "hidden",
@@ -92,15 +92,20 @@ const PDFToExport = ({ favoriteColors }) => {
                       <View
                         style={[
                           styles.colorImage,
-                          {
+                          {/*
                             border: `10px solid rgb(${color[COLORINFO.rgbR]},${color[COLORINFO.rgbG]},${
                               color[COLORINFO.rgbB]
-                            })`,
+                            })`,*/
+                            backgroundColor:
+                            `rgb(
+                                ${color[COLORINFO.rgbR]},
+                                ${color[COLORINFO.rgbG]},
+                                ${color[COLORINFO.rgbB]})`
                           },
                         ]}
                       >
                         {/* FALTA IMÁGEN */}
-                        {/* <Image style={styles.image} src={color[5]} /> */}
+                        <View style={styles.image} src={color[5]}></View>
                       </View>
                     </View>
                   </View>
@@ -189,7 +194,7 @@ const ExportPDF = ({ setDownloaded, favoriteColors }) => {
   return (
     <>
       {isClient ? (
-        <div className="text-white">
+        <div className={`text-white text-xs flex flex-col items-center justify-center`}>
           <PDFDownloadLink
             onClick={() => {
               setDownloaded(true);
@@ -197,7 +202,9 @@ const ExportPDF = ({ setDownloaded, favoriteColors }) => {
             document={<PDFToExport favoriteColors={favoriteColors} />}
             fileName="colores-favoritos.pdf"
           >
-            {({ blob, url, loading, error }) => (loading ? "Cargando colores..." : "Exportar")}
+            {({ blob, url, loading, error }) => (loading ? "Cargando colores..." : <><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-cloud-arrow-down-fill" viewBox="0 0 16 16">
+  <path d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2m2.354 6.854-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5a.5.5 0 0 1 1 0v3.793l1.146-1.147a.5.5 0 0 1 .708.708z"/>
+</svg> </>)}
           </PDFDownloadLink>
         </div>
       ) : (
